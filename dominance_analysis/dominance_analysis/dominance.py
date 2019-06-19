6fbc8467979e350368b80f4ad5f5d34c0e1c7e28
Bala Koteshwar Kolluri <49233966+balakolluri@users.noreply.github.com>
2019-06-19 10:27:27 +0530

    Update dominance.py

--- dominance_analysis/dominance_analysis/dominance.py
+++ dominance_analysis/dominance_analysis/dominance.py
@@ -5,13 +5,14 @@ from sklearn.linear_model import LinearRegression
 from sklearn.datasets import load_breast_cancer
 from sklearn.datasets import load_boston
 from tqdm import tqdm
-from sklearn.feature_selection import SelectKBest,chi2,f_regression
+from sklearn.feature_selection import SelectKBest,chi2,f_regression,f_classif
 import pandas as pd
 from plotly import offline
 from plotly.offline import init_notebook_mode,iplot
 import cufflinks as cf
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import log_loss
+from scipy.misc import factorial
 import statsmodels.api as sm
 from functools import  reduce
 import math
@@ -246,10 +247,15 @@ class Dominance:
 		# remove intercept from top_k
 		if(self.objective):
 			top_k_vars=SelectKBest(f_regression, k=self.top_k)
+			top_k_vars.fit_transform(self.data[columns], self.data[self.target])
 		else:
 			columns.remove('intercept')
-			top_k_vars=SelectKBest(chi2, k=self.top_k)
-		top_k_vars.fit_transform(self.data[columns], self.data[self.target])
+			try:
+				top_k_vars=SelectKBest(chi2, k=self.top_k)
+				top_k_vars.fit_transform(self.data[columns], self.data[self.target])
+			except:
+				top_k_vars=SelectKBest(f_classif, k=self.top_k)
+				top_k_vars.fit_transform(self.data[columns], self.data[self.target])
 		return [columns[i] for i in top_k_vars.get_support(indices=True)]
 
 	def plot_waterfall_relative_importance(self,incremental_rsquare_df):
@@ -555,5 +561,3 @@ class Dominance_Datasets:
 
 	def __init__(self):
 		print("Datasets for Dominance Analysis")
-		
-
